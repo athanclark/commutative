fcb24e966b0428645dd98ac6506cf98f080f377d
Athan Clark <athan.clark@gmail.com>
2015-07-27 18:55:41 -0600

    more progress

--- src/Data/Commutative.hs
+++ src/Data/Commutative.hs
@@ -5,3 +5,9 @@ module Data.Commutative where
 
 class Commutative a where
   commute :: a -> a -> a -- ^ Abelian binary operation - @x `commute` y == y `commute` x@
+
+(<~>) :: Commutative a => a -> a -> a
+(<~>) = commute
+
+class Commutative a => CommutativeId a where
+  cempty :: a -- ^ Identity element - @x `commute` cempty == cempty `commute` x == x@
---
+++ src/Data/Mergeable.hs
@@ -0,0 +1,11 @@
+module Data.Mergeable where
+
+import Data.Commutative
+
+
+class Mergeable t where
+  mergeMap :: CommutativeId m => (a -> m) -> t a -> m
+  merge :: (a -> b -> b) -> b -> t a -> b
+
+  mergeMap f = merge (commute . f) cempty
+  merge f i xs = 
